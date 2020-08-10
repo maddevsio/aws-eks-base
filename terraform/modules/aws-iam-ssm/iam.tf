027c9a746453fc8c568e3dc8cbe91d146eccc60e
Andrew S <halfb00t@gmail.com>
2020-08-10 11:33:29 +0600

    #2 Added iam for extarnal secrets

--- terraform/modules/aws-iam-ssm/iam.tf
+++ terraform/modules/aws-iam-ssm/iam.tf
@@ -32,7 +32,7 @@ data "aws_iam_policy_document" "this" {
     effect = "Allow"
 
     actions = [
-      "ecr:*",
+      "ssm:*",
     ]
 
     resources = ["*"]
---
+++ terraform/modules/aws-iam-ssm/variables.tf
@@ -0,0 +1,18 @@
+variable "oidc_provider_arn" {
+  description = "The ARN of the OIDC Provider"
+  default     = ""
+}
+variable "name" {
+  description = "Project name, required to form unique resource names"
+  default     = ""
+}
+
+variable "region" {
+  description = "Target region for all infrastructure resources"
+  default     = ""
+}
+
+variable "eks_cluster_id" {
+  description = "ID of EKS cluster"
+  default     = ""
+}
