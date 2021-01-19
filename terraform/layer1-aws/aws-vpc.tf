c528b5e97d9d1b57087bd60644a885dd9b4a2294
Maxim Glotov <nikromax@gmail.com>
2021-01-19 04:42:15 +0000

    added vpc_s3_endpoint to reduce traffic costs

--- terraform/layer1-aws/aws-vpc.tf
+++ terraform/layer1-aws/aws-vpc.tf
@@ -27,6 +27,8 @@ module "vpc" {
   enable_dns_hostnames = true
   enable_dns_support   = true
 
+  enable_s3_endpoint   = true
+
   create_database_subnet_group = false
 
   manage_default_security_group  = true
