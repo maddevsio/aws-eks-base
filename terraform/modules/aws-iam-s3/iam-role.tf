4ef71b508d3dc21b9d913e8458191d9b5bc12df9
Aleksandr Zhitov <aleksandr.jitov@gmail.com>
2021-03-05 18:16:57 +0600

    Small fixes

--- terraform/modules/aws-iam-s3/iam-role.tf
+++ terraform/modules/aws-iam-s3/iam-role.tf
@@ -10,17 +10,17 @@ resource "aws_iam_role" "this" {
 data "aws_iam_policy_document" "role_policy" {
 
   statement {
-      actions = ["sts:AssumeRoleWithWebIdentity"]
-      effect  = "Allow"
-      principals {
-        type        = "Federated"
-        identifiers = [var.oidc_provider_arn]
-      }
-      condition {
-        test     = "StringEquals"
-        variable = "oidc.eks.${var.region}.amazonaws.com/id/${regex("[A-Z0-9]{32}", var.oidc_provider_arn)}:aud"
-        values   =  ["sts.amazonaws.com"]
-      }
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+    effect  = "Allow"
+    principals {
+      type        = "Federated"
+      identifiers = [var.oidc_provider_arn]
+    }
+    condition {
+      test     = "StringEquals"
+      variable = "oidc.eks.${var.region}.amazonaws.com/id/${regex("[A-Z0-9]{32}", var.oidc_provider_arn)}:aud"
+      values   = ["sts.amazonaws.com"]
+    }
   }
 }
 
@@ -38,29 +38,29 @@ resource "aws_iam_role_policy" "this" {
 data "aws_iam_policy_document" "policy" {
 
   statement {
-      actions   = [
-        "s3:ListBucket",
-        "s3:GetBucketLocation",
-        "s3:ListBucketMultipartUploads",
-        "s3:ListBucketVersions"
-      ]
-      effect    = "Allow"
-      resources = [
-        for buckets in var.bucket_names:
-        "arn:aws:s3:::${buckets}"]
+    actions = [
+      "s3:ListBucket",
+      "s3:GetBucketLocation",
+      "s3:ListBucketMultipartUploads",
+      "s3:ListBucketVersions"
+    ]
+    effect = "Allow"
+    resources = [
+      for buckets in var.bucket_names :
+    "arn:aws:s3:::${buckets}"]
   }
 
   statement {
-      actions   = [
-        "s3:GetObject",
-        "s3:PutObject",
-        "s3:DeleteObject",
-        "s3:AbortMultipartUpload",
-        "s3:ListMultipartUploadParts"
-      ]
-      effect    = "Allow"
-      resources = [
-        for buckets in var.bucket_names:
-        "arn:aws:s3:::${var.bucket_names}/${var.path}*"]
+    actions = [
+      "s3:GetObject",
+      "s3:PutObject",
+      "s3:DeleteObject",
+      "s3:AbortMultipartUpload",
+      "s3:ListMultipartUploadParts"
+    ]
+    effect = "Allow"
+    resources = [
+      for buckets in var.bucket_names :
+    "arn:aws:s3:::${var.bucket_names}/${var.path}*"]
   }
 }
--- terraform/modules/aws-iam-s3/iam-user.tf
+++ terraform/modules/aws-iam-s3/iam-user.tf
@@ -23,30 +23,30 @@ resource "aws_iam_user_policy" "this" {
 data "aws_iam_policy_document" "policy" {
 
   statement {
-      actions = [
-        "s3:ListBucket",
-        "s3:GetBucketLocation",
-        "s3:ListBucketMultipartUploads",
-        "s3:ListBucketVersions"
-      ]
-      effect = "Allow"
-      resources = [
-        for buckets in var.bucket_names:
-        "arn:aws:s3:::${buckets}"]
+    actions = [
+      "s3:ListBucket",
+      "s3:GetBucketLocation",
+      "s3:ListBucketMultipartUploads",
+      "s3:ListBucketVersions"
+    ]
+    effect = "Allow"
+    resources = [
+      for buckets in var.bucket_names :
+    "arn:aws:s3:::${buckets}"]
   }
 
   statement {
-      actions = [
-        "s3:GetObject",
-        "s3:PutObject",
-        "s3:DeleteObject",
-        "s3:AbortMultipartUpload",
-        "s3:ListMultipartUploadParts"
-      ]
-      effect = "Allow"
-      resources = [
-        for buckets in var.bucket_names:
-        "arn:aws:s3:::${buckets}/${var.path}*"]
+    actions = [
+      "s3:GetObject",
+      "s3:PutObject",
+      "s3:DeleteObject",
+      "s3:AbortMultipartUpload",
+      "s3:ListMultipartUploadParts"
+    ]
+    effect = "Allow"
+    resources = [
+      for buckets in var.bucket_names :
+    "arn:aws:s3:::${buckets}/${var.path}*"]
   }
 }
 
