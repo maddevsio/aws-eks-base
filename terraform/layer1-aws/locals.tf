3dc8d947777d018a57e9a8be2c9ad82ff7acc6f3
Andrew S <halfb00t@gmail.com>
2021-04-15 17:11:42 +0600

    #26 added r53 module to get or create domain name

--- terraform/layer1-aws/locals.tf
+++ terraform/layer1-aws/locals.tf
@@ -5,8 +5,8 @@ locals {
   name           = "${var.name}-${local.env}-${local.short_region}"
   name_wo_region = "${var.name}-${local.env}"
   domain_name    = var.domain_name
-  zone_id        = data.aws_route53_zone.main.zone_id
   account_id     = data.aws_caller_identity.current.account_id
 
   ssl_certificate_arn = var.create_acm_certificate ? module.acm.this_acm_certificate_arn : data.aws_acm_certificate.main[0].arn
+  zone_id             = var.create_r53_zone ? keys(module.r53_zone.this_route53_zone_zone_id)[0] : (var.zone_id != null ? var.zone_id : data.aws_route53_zone.main[0].zone_id)
 }
--- terraform/layer1-aws/main.tf
+++ terraform/layer1-aws/main.tf
@@ -16,8 +16,3 @@ terraform {
 data "aws_availability_zones" "available" {}
 
 data "aws_caller_identity" "current" {}
-
-data "aws_route53_zone" "main" {
-  name         = "${var.domain_name}."
-  private_zone = false
-}
