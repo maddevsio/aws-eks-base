914b04f0cf55395e1f95404ccc67930428141c1f
Khalezin Dmitrii <khalezin.d@gmail.com>
2021-06-03 12:49:28 +0300

    #62 fixed namesapce for loki stack

--- terraform/layer2-k8s/eks-loki-stack.tf
+++ terraform/layer2-k8s/eks-loki-stack.tf
@@ -15,7 +15,7 @@ resource "helm_release" "loki_stack" {
   name        = "loki-stack"
   chart       = "loki-stack"
   repository  = local.helm_repo_grafana
-  namespace   = "monitoring"
+  namespace   = kubernetes_namespace.monitoring.id
   version     = var.loki_stack
   wait        = false
   max_history = var.helm_release_history_size
