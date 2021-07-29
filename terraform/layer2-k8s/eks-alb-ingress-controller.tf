5356b3b8651b89cc93ebc23d0f804df8c0a78046
Khalezin Dmitrii <khalezin.d@gmail.com>
2021-07-29 14:08:25 +0600

    #73 fixed path to module

--- terraform/layer2-k8s/eks-alb-ingress-controller.tf
+++ terraform/layer2-k8s/eks-alb-ingress-controller.tf
@@ -1,5 +1,5 @@
 module "eks_alb_ingress" {
-  source = "./modules/eks-alb-ingress"
+  source = "../modules/eks-alb-ingress"
   count  = var.alb_ingress_enable ? 1 : 0
 
   name              = local.name
