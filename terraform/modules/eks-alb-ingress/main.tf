e3bb697fe1ef18607efa637bc59dcaf5fec37543
vitt <vitt@asus.com>
2021-07-29 11:08:25 +0600

    Added module alb for alb ingress #73 https://github.com/maddevsio/aws-eks-base/issues/73#issue-890895211

---
+++ terraform/modules/eks-alb-ingress/main.tf
@@ -0,0 +1,20 @@
+module "aws_iam_alb_ingress_controller" {
+  source            = "../aws-iam-alb-ingress-controller"
+  name              = var.name
+  region            = var.region
+  oidc_provider_arn = var.oidc_provider_arn
+}
+
+resource "helm_release" "alb_ingress_controller" {
+  name             = var.release_name
+  chart            = var.chart_name
+  repository       = var.repository
+  version          = var.chart_version
+  namespace        = var.namespace
+  create_namespace = true
+  max_history      = var.max_history
+  values = [
+    local.alb_ingress_controller
+  ]
+}
+
