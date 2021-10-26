160adcf033ec89b087923a2d650b3721cb635153
maxim <nikromax@gmail.com>
2021-10-26 15:59:33 +0600

    fix: fix aws-iam-user-with-policy terraform module

--- terraform/modules/aws-iam-user-with-policy/iam-user.tf
+++ terraform/modules/aws-iam-user-with-policy/iam-user.tf
@@ -12,34 +12,3 @@ resource "aws_iam_user_policy" "this" {
 
   policy = var.policy
 }
-
-data "aws_iam_policy_document" "user_policy" {
-
-  statement {
-    actions = [
-      "s3:ListBucket",
-      "s3:GetBucketLocation",
-      "s3:ListBucketMultipartUploads",
-      "s3:ListBucketVersions"
-    ]
-    effect = "Allow"
-    resources = [
-      for buckets in var.bucket_names :
-    "arn:aws:s3:::${buckets}"]
-  }
-
-  statement {
-    actions = [
-      "s3:GetObject",
-      "s3:PutObject",
-      "s3:DeleteObject",
-      "s3:AbortMultipartUpload",
-      "s3:ListMultipartUploadParts"
-    ]
-    effect = "Allow"
-    resources = [
-      for buckets in var.bucket_names :
-    "arn:aws:s3:::${buckets}/${var.path}*"]
-  }
-}
-
