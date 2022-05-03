34f6f7137d1d43dd852c29784acdbc0ad84bb392
maxim <nikromax@gmail.com>
2022-05-03 11:11:35 +0600

    fix vpc-cni IAM role name

--- terraform/layer1-aws/aws-iam.tf
+++ terraform/layer1-aws/aws-iam.tf
@@ -2,7 +2,7 @@ module "vpc_cni_irsa" {
   source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
   version = "4.14.0"
 
-  role_name             = "${local.name}-vpc_cni"
+  role_name             = "${local.name}-vpc-cni"
   attach_vpc_cni_policy = true
   vpc_cni_enable_ipv4   = true
 
