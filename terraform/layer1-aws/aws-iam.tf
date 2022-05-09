08b091812c89f8640717bec915170c8d13c3f49f
maxim <nikromax@gmail.com>
2022-05-09 17:36:13 +0600

    enh: configure AWS account password policy

--- terraform/layer1-aws/aws-iam.tf
+++ terraform/layer1-aws/aws-iam.tf
@@ -1,3 +1,16 @@
+resource "aws_iam_account_password_policy" "default" {
+  count = var.aws_account_password_policy.create ? 1 : 0
+
+  minimum_password_length        = var.aws_account_password_policy.minimum_password_length
+  password_reuse_prevention      = var.aws_account_password_policy.password_reuse_prevention
+  require_lowercase_characters   = var.aws_account_password_policy.require_lowercase_characters
+  require_numbers                = var.aws_account_password_policy.require_numbers
+  require_uppercase_characters   = var.aws_account_password_policy.require_uppercase_characters
+  require_symbols                = var.aws_account_password_policy.require_symbols
+  allow_users_to_change_password = var.aws_account_password_policy.allow_users_to_change_password
+  max_password_age               = var.aws_account_password_policy.max_password_age
+}
+
 module "vpc_cni_irsa" {
   source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
   version = "4.14.0"
