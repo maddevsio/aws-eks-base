2f456081d2ee504b70112f1d983c7725cd273a46
Andrew S <halfb00t@gmail.com>
2022-07-18 16:02:36 +0600

    feat: removed data remote_state #253

--- terraform/layer2-k8s/locals.tf
+++ terraform/layer2-k8s/locals.tf
@@ -1,19 +1,19 @@
 locals {
   region                = var.region
-  short_region          = data.terraform_remote_state.layer1-aws.outputs.short_region
-  az_count              = data.terraform_remote_state.layer1-aws.outputs.az_count
-  name                  = data.terraform_remote_state.layer1-aws.outputs.name
-  env                   = data.terraform_remote_state.layer1-aws.outputs.env
-  name_wo_region        = data.terraform_remote_state.layer1-aws.outputs.name_wo_region
-  zone_id               = data.terraform_remote_state.layer1-aws.outputs.route53_zone_id
-  domain_name           = data.terraform_remote_state.layer1-aws.outputs.domain_name
-  domain_suffix         = "${local.env}.${local.domain_name}"
-  allowed_ips           = data.terraform_remote_state.layer1-aws.outputs.allowed_ips
+  short_region          = var.short_region[var.region]
+  name                  = "${var.name}-${local.env}-${local.short_region}"
+  name_wo_region        = "${var.name}-${local.env}"
+  env                   = var.environment
+  zone_id               = var.zone_id
+  domain_name           = var.domain_name
+  domain_suffix         = "${local.env}.${var.domain_name}"
+  allowed_ips           = var.allowed_ips
   ip_whitelist          = join(",", concat(local.allowed_ips, var.additional_allowed_ips))
-  vpc_id                = data.terraform_remote_state.layer1-aws.outputs.vpc_id
-  vpc_cidr              = data.terraform_remote_state.layer1-aws.outputs.vpc_cidr
-  eks_cluster_id        = data.terraform_remote_state.layer1-aws.outputs.eks_cluster_id
-  eks_oidc_provider_arn = data.terraform_remote_state.layer1-aws.outputs.eks_oidc_provider_arn
+  vpc_id                = var.vpc_id
+  vpc_cidr              = var.vpc_cidr
+  eks_cluster_id        = var.eks_cluster_id
+  eks_oidc_provider_arn = var.eks_oidc_provider_arn
+  ssl_certificate_arn   = var.ssl_certificate_arn
 
   helm_releases = yamldecode(file("${path.module}/helm-releases.yaml"))["releases"]
 }
--- terraform/layer2-k8s/main.tf
+++ terraform/layer2-k8s/main.tf
@@ -25,15 +25,4 @@ terraform {
   }
 }
 
-data "terraform_remote_state" "layer1-aws" {
-  backend = "s3"
-  config = {
-    bucket  = var.remote_state_bucket
-    key     = "${var.remote_state_key}/terraform.tfstate"
-    region  = var.region
-    encrypt = "true"
-  }
-  workspace = terraform.workspace
-}
-
 data "aws_caller_identity" "current" {}
