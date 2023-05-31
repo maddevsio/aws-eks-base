8d5fac9e4f902fcc1cf9de57c4255ee51c016453
vitalii v v <87434848+vitaliimd@users.noreply.github.com>
2023-05-31 17:27:41 +0600

    fixed terraform docs resources links

--- terraform/layer2-k8s/main.tf
+++ terraform/layer2-k8s/main.tf
@@ -3,15 +3,15 @@ terraform {
 
   required_providers {
     aws = {
-      source  = "aws"
+      source  = "hashicorp/aws"
       version = "4.62.0"
     }
     kubernetes = {
-      source  = "kubernetes"
+      source  = "hashicorp/kubernetes"
       version = "2.19.0"
     }
     helm = {
-      source  = "helm"
+      source  = "hashicorp/helm"
       version = "2.6.0"
     }
     http = {
