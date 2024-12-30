9600310d27ccd51d4253df35d8e3c6bcdc7689eb
Valentin Khramtsov <valentin.kh@maddevs.io>
2024-12-30 16:03:30 +0300

    Tflint for aws-iam-user-with-policy module

--- terraform/modules/aws-iam-user-with-policy/variables.tf
+++ terraform/modules/aws-iam-user-with-policy/variables.tf
@@ -1,7 +1,9 @@
 variable "name" {
+  type        = string
   description = "Project name, required to form unique resource names"
 }
 
 variable "policy" {
+  type        = string
   description = "IAM policy that will be attached to user"
 }
