c89dacd300f7e307d57ec9b1f46d7351782a3b7d
Valentin Khramtsov <valentin.kh@maddevs.io>
2024-12-30 15:52:11 +0300

    Tflint for aws-eks module

--- terraform/modules/aws-iam-eks-trusted/variables.tf
+++ terraform/modules/aws-iam-eks-trusted/variables.tf
@@ -1,15 +1,19 @@
 variable "oidc_provider_arn" {
+  type        = string
   description = "The ARN of the OIDC Provider"
 }
 
 variable "name" {
+  type        = string
   description = "Name, required to form unique resource names"
 }
 
 variable "region" {
+  type        = string
   description = "Target region for all infrastructure resources"
 }
 
 variable "policy" {
+  type        = string
   description = "The policy that will be attached to role"
 }
